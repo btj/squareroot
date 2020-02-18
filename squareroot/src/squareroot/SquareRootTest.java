033756cc1dc6ef8a33a7ea453d29db2243f7f74a
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2020-02-18 17:38:20 +0100

    Initial commit

---
+++ squareroot/src/squareroot/SquareRootTest.java
@@ -0,0 +1,17 @@
+package squareroot;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SquareRootTest {
+	
+	@Test
+	void test() {
+		assert SquareRoot.squareRoot(0) == 0;
+		assert SquareRoot.squareRoot(9) == 3;
+		assert SquareRoot.squareRoot(10) == 3;
+		assert SquareRoot.squareRoot(16) == 4;
+	}
+
+}
